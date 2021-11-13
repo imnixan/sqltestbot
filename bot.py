be6b3aade79ab4779714af4f30cf311f95d8820f
imnixan <vcmvenom@gmail.com>
2021-11-13 09:52:27 +0300

    123

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import re
-from sqlcommands import newtablenote, showsqluser
+from sqlcommands import newtablenote, showsqluser, delsqluser
 import logging
 import config
 from config import host, user, password, db_name
@@ -15,8 +15,8 @@ dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
 
 
-# запуск бота
-@dp.message_handler(commands="start")
+#первый запуск
+@dp.message_handler(commands="/start")
 async def cmd_test1(message: types.Message):
     answer = newtablenote('telegramid', str(message.from_user.id))
     if (answer[0] == "Успешно"):
@@ -27,15 +27,30 @@ async def cmd_test1(message: types.Message):
 
 
 
-#вывод пользователя
+#ловим сообщения
 @dp.message_handler()
-async def showuser(message: types.Message):
+async def messagehandler(message: types.Message):
     
     if (re.search('shw.usr', message.text) and message.from_user.id == 268026070):
-        answer = showsqluser(re.search('(?<=shw\.usr ).*(?=\.)', str(message.text)).group(0),re.search('(?<=\w\.)(\w|\d)+$', str(message.text)).group(0))
+        print("функция выполняется")
+        answer = showsqluser(re.search('(?<=\.usr ).*(?=\()', str(message.text)).group(0), re.search('(?<=\().*(?=\))', str(message.text)).group(0))
         if (answer[0] == "Успешно"):
-            print (answer)
-            await  message.answer ("Пользователь найден: " + str(answer[1]))
-       
-
+            await  message.answer (f"""Пользователь найден:
+            Имя - {answer[1][0]}
+            Дата рождения - {answer[1][1]}
+            Пол - {answer[1][2]}
+            Ищет - {answer[1][6]}
+            Описание - {answer[1][3]}
+            Telegram Id - {answer[1][5]}
+            {answer[1][4]}"""
+            )
+            if (answer[1][4] == None):
+                print ('фотки нет')
+    
+    elif (re.search('del.usr', message.text) and message.from_user.id == 268026070):
+        print("функция выполняется")
+        answer = delsqluser(re.search('(?<=\.usr ).*(?=\()', str(message.text)).group(0), re.search('(?<=\().*(?=\))', str(message.text)).group(0))
+        if (answer == "Успешно"):
+            await  message.answer ("Пользователь удален")
+            
 executor.start_polling(dp, skip_updates=True)
